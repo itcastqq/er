efad5d179c4dbe463f39553de2ac009a09c17802
itcast <bos@itcast.cn>
2019-03-30 19:41:57 +0800

    分为

--- day01/src/test/java/test.java
+++ day01/src/test/java/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("实时");
-		
+        System.out.println("wefqwef ");
+        System.out.println();
     }
 }
