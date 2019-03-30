b5f76f691e861a443e2c2a4220393f32ea32b4f2
itcast <bos@itcast.cn>
2019-03-30 19:45:03 +0800

    瑟夫

--- day01/src/test/java/test.java
+++ day01/src/test/java/test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("wefqwef ");
+        System.out.println("wefqwef wefq wef速度范围e");
+        System.out.println("wefawef啊热狗");
         System.out.println();
     }
 }
