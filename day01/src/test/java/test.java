a219decaf293b57d6f8803d59dc97e2b7e1ddaab
itcast <bos@itcast.cn>
2019-03-30 19:44:12 +0800

    位符

--- day01/src/test/java/test.java
+++ day01/src/test/java/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("wefqwef ");
+        System.out.println("wefqwef wefq wef速度范围e");
         System.out.println();
         System.out.println();
     }
